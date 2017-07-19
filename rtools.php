19b402adb526278145d2972574c57b30ad502a72
enridaga <enricodaga@gmail.com>
2017-07-19 10:24:53 +0100

    Updated relevant rules

--- rtools.php
+++ rtools.php
@@ -79,6 +79,52 @@ function select(){
     }
 }
 
+function relevance( $c_trashold = 0.9, $s_trashold = 0.3){
+	global $rulesFilename;
+    $handle = fopen($rulesFilename,'r');
+	$dimensions = array();
+	if($handle){
+      while ( ($line = fgetcsv ( $handle )) !== false ) {
+		  $rule = $line;
+		  // 2 support
+		  // 3 confidence
+		  if(floatVal($rule[2]) < $s_trashold) continue;
+		  if(floatVal($rule[3]) < $c_trashold) continue;
+		  if(!isset($dimensions[$rule[0]])){
+		  	$dimensions[$rule[0]] = $rule;
+		  }else{
+			  if(floatVal($rule[3]) > floatVal($dimensions[$rule[0]][3])){
+  			  	$dimensions[$rule[0]] = $rule;
+			  }else if(floatVal($rule[3]) == floatVal($dimensions[$rule[0]][3])){
+				  // Check Support then
+				  if(floatVal($rule[2]) > floatVal($dimensions[$rule[0]][2])){
+	  			  	$dimensions[$rule[0]] = $rule;
+				  }
+			  }
+		  }
+      }
+      fclose($handle);
+    }else{
+      throw new Exception('File not found');
+    }
+	usort($dimensions, function($a, $b) { 
+		$aa = $a[3];
+		$bb = $b[3];
+		if ($aa == $bb){
+			// Check support
+			$aa = $a[2];
+			$bb = $b[2];
+			if($aa == $bb){
+				return 0;
+			}
+		}
+		return ($aa < $bb) ? 1 : -1; 
+	});
+	foreach($dimensions as $k => $d){
+		print $d[3] . "\t" . $d[2] . "\t" . $d[0] . ' <- ' . $d[1] . "\n";
+	}
+}
+
 function rule($rule){
 	print "(" . $rule[0] . ")\n\t <- (" .$rule[1].")\n\t (s: " . $rule[2] . ", c: " .$rule[3] .")\n";
 }
